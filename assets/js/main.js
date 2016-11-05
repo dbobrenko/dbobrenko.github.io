88da65e4a60360b3c5368aa0e21b832f4472aa3c
Alexandr Bobrenko <oknerbob@gmail.com>
2016-11-05 18:58:42 +0200

    added video styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,14 +14,32 @@ var pageController = {
 
   /** Page functions **/
   pageHandlers: function (){
-    var postLinks = document.querySelectorAll('.page-content a');
-    var linksLen = postLinks.length;
 
-    for(var i = 0; i < linksLen; i++){
-      if(postLinks[i].getAttribute('href').indexOf('http') != -1) {
-        postLinks[i].setAttribute('target', '_blank');
+    externalLinksInNewWindow(); // Open external links in new window
+    resizeVideo(); // Resize ".video" class on different screens
+
+    /*** Open external links in new window ***/
+    function externalLinksInNewWindow(){
+      var postLinks = document.querySelectorAll('.page-content a');
+      var linksLen = postLinks.length;
+
+      for(var i = 0; i < linksLen; i++){
+        if(postLinks[i].getAttribute('href').indexOf('http') != -1) {
+          postLinks[i].setAttribute('target', '_blank');
+        }
+      };
+    }
+
+    /*** Resize ".video" class on different screens ***/
+    function resizeVideo(){
+      var videoElements = document.querySelectorAll('.page-content .video > iframe');
+
+      for(var i = 0; i < videoElements.length; i++){
+        var width = videoElements[i].offsetWidth; // get iframe width
+        var height = (width * 0.75).toPrecision(1); // calc hight with 4:3 ratio
+        videoElements[i].style.height = height + 'px'; // set height
       }
-    };
+    }
   }
 };
 
