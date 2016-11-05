08253c893c9fad3c6073fd42930389a26fbe0950
Alexandr Bobrenko <oknerbob@gmail.com>
2016-11-05 17:19:20 +0200

    all external links in posts will be opened in new window

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,19 @@ var pageController = {
   init: function (){
     navigation.init(); // initialization of main (top) menu
     modal.init(); // initialization of modal form controller
+    this.pageHandlers(); // init different page functions
+  },
+
+  /** Page functions **/
+  pageHandlers: function (){
+    var postLinks = document.querySelectorAll('.page-content a');
+    var linksLen = postLinks.length;
+
+    for(var i = 0; i < linksLen; i++){
+      if(postLinks[i].getAttribute('href').indexOf('http') != -1) {
+        postLinks[i].setAttribute('target', '_blank');
+      }
+    };
   }
 };
 
